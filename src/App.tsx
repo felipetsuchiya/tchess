604e1c5e0ac40e1b515e052a01450517a1d02457
felipetsuchiya <felipetsuchiya@hotmail.com>
2026-05-13 16:14:11 -0300

    add: first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
